9a6a32e2aa680130ffb5a8edc709a020eb16f0b9
Dzmitry Bryt <bdv212@yandex.ru>
2020-10-30 14:22:48 +0300

    feat: init project

---
+++ src/api/youtube.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export default axios.create({
+   baseURL: 'https://www.googlepapis.com/youtube/v3',
+   params: {
+      parts: 'snippets',
+      maxResults: 5,
+      
+   }
+}); 
+
